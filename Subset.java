9320f19548806c7f026e4961f83a4577925bf9e2
Mona-B <monabalasubramanian13@gmail.com>
2016-07-06 01:41:48 +0530

    checking whether array a2 is a subset of array a1

--- Subset.java
+++ Subset.java
@@ -10,14 +10,15 @@ import java.util.ArrayList;
 
 public class Subset {
 	public static void main(String args[]) {
-		// Array1 is converted into a list
 		int[] a1 = { 1, 2, 3 };
+		int[] a2 = { 2, 3 };
+		if(a1.length!=0 && a2.length!=0){
+		// Array1 is converted into a list
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		for (int index = 0; index < a1.length; index++) {
 			intList.add(a1[index]);
 		}
 		// Array2 is converted into a list
-		int[] a2 = { 2, 3 };
 		ArrayList<Integer> intList1 = new ArrayList<Integer>();
 		for (int index = 0; index < a2.length; index++) {
 			intList1.add(a2[index]);
@@ -27,6 +28,9 @@ public class Subset {
 			System.out.println("a2 is the subset of a1");
 		} else {
 			System.out.println("a2 is not the subset of a1");
+		}
+		}else{
+			System.out.println("One of the array is empty")
 		}
 	}
 }
